3b7fab7ca0b363690bf1552a9260204745a3f814
Alex Collins <alex.e.c@gmail.com>
2017-04-09 17:35:10 +0100

    fixes #34

--- patch-epub.sh
+++ patch-epub.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 #
-# This script removed not allowed "data-lang" attribute from HTML files.
 set -eux
 
 O=$(pwd)
@@ -10,7 +9,12 @@ cd $T
 
 unzip -d . $O/book.epub
 
+# remove not allowed "data-lang" attribute from HTML files.
 find . -type f -name '*.html' | xargs sed -i.bak 's/data-lang="[^"]*"//g'
+find . -type f -name '*.bak' | xargs rm
+
+# change "_target" to "target"
+find . -type f -name '*.html' | xargs sed -i.bak 's/_target/target/g'
 find . -type f -name '*.bak' | xargs rm
 
 zip -u $O/book.epub *
